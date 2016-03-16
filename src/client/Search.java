4f8c5bf45a627fc3e3e01017a471a89319690e2c
Balazs Pekar <peba0706@gmail.com>
2016-03-16 16:06:55 +0100

    Code complies and somewhat working

--- src/client/Search.java
+++ src/client/Search.java
@@ -1,7 +1,10 @@
 package client;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+import java.net.Socket;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -19,7 +22,11 @@ public class Search extends HttpServlet
 
 		String query = request.getParameter("query");
 
-		out.println(query);
+		Socket client = new Socket("localhost", 3232);
+		ObjectOutputStream objectToServerSide = new ObjectOutputStream(client.getOutputStream());
+		ObjectInputStream objectFromServerSide = new ObjectInputStream(client.getInputStream());
+
+		objectToServerSide.writeObject(query);
 	}
 
 }
--- src/server/PersonStoreServerSocket.java
+++ src/server/PersonStoreServerSocket.java
@@ -1,5 +1,9 @@
 package server;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Set;
 
 import server.csvhandling.CSVDataReader;
@@ -14,10 +18,28 @@ public class PersonStoreServerSocket
 
 	public void start()
 	{
-		store.setSearchType(SearchType.OPTIONAL);
-		store.setSerachCriteria("Java");
-		Set<Person> filteredPersons = store.getPersons();
-		System.out.println(filteredPersons);
+
+		try
+		{
+			ServerSocket storageServer = new ServerSocket(3232);
+			Socket server = storageServer.accept();
+			ObjectInputStream objectFromClientSide = new ObjectInputStream(server.getInputStream());
+			ObjectOutputStream objectToClientSide = new ObjectOutputStream(server.getOutputStream());
+
+			String queryFromClient = (String) objectFromClientSide.readObject();
+			System.out.println("This is is the query received from the Client: ");
+
+			store.setSearchType(SearchType.OPTIONAL);
+			store.setSerachCriteria(queryFromClient);
+			Set<Person> filteredPersons = store.getPersons();
+			System.out.println(filteredPersons);
+
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
 	}
 
 	public static void main(String[] args)
